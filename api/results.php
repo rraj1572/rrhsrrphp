cbe6145e6a26e183dabacb0c3f1ae311ccd8b38f
rraj1572 <91118622+rraj1572@users.noreply.github.com>
2023-06-10 19:18:03 +0530

    Update results.php

--- api/results.php
+++ api/results.php
@@ -1,73 +1,22 @@
 <?php
-// disable error
-error_reporting(0);
-$error = 2;
 
-// Encode Key ID
-$hex = $_GET["keyid"];            // and much more hex values as string as in your example
-$bin = hex2bin($hex);       // convert the hex values to binary data stored as a PHP string
-$keyid64 = base64_encode($bin); // remove == , replace as space
-$finalkeyid64 = str_replace('=', '', $keyid64);
+$url = $_GET['url'];
 
+$text = '#EXTM3U
+#EXT-X-INDEPENDENT-SEGMENTS
+#EXT-X-VERSION:3
+#EXT-X-STREAM-INF:BANDWIDTH=198000,AVERAGE-BANDWIDTH=180400,RESOLUTION=320x180,FRAME-RATE=25,CODECS="avc1.42c00d,mp4a.40.2"'
+.$url.'/master_1.m3u8
+#EXT-X-STREAM-INF:BANDWIDTH=325600,AVERAGE-BANDWIDTH=290400,RESOLUTION=426x240,FRAME-RATE=25,CODECS="avc1.4d4015,mp4a.40.2"'
+.$url.'/master_2.m3u8
+#EXT-X-STREAM-INF:BANDWIDTH=389400,AVERAGE-BANDWIDTH=345400,RESOLUTION=640x360,FRAME-RATE=25,CODECS="avc1.77.30,mp4a.40.2"'
+.$url.'/master_3.m3u8
+#EXT-X-STREAM-INF:BANDWIDTH=555280,AVERAGE-BANDWIDTH=488400,RESOLUTION=854x480,FRAME-RATE=25,CODECS="avc1.4d401f,mp4a.40.2"'
+.$url.'/master_4.m3u8
+#EXT-X-STREAM-INF:BANDWIDTH=1856800,AVERAGE-BANDWIDTH=1610400,RESOLUTION=1280x720,FRAME-RATE=25.000,CODECS="avc1.640028,mp4a.40.2"'
+.$url.'/master_5.m3u8
+#EXT-X-STREAM-INF:BANDWIDTH=3898400,AVERAGE-BANDWIDTH=3370400,RESOLUTION=1920x1080,FRAME-RATE=25.000,CODECS="avc1.640028,mp4a.40.2"'
+.$url.'/master_6.m3u8';
 
-// validation
-if (empty($finalkeyid64)){
-
-    // return 503 & JSON
-   http_response_code(503);
-header("Content-Type: application/json");
-$errorjson = array("Status"=>"503","Content"=>"Validation Failed!","Reason"=>"Did not provide Key ID | Key or Key ID | Key isn't complete.");
-echo json_encode($errorjson);
-$error = 1;
-   
-   exit;
-}
-
-// Encode Key
-$hex2 = $_GET["key"];            // and much more hex values as string as in your example
-$bin2 = hex2bin($hex2);       // convert the hex values to binary data stored as a PHP string
-$key64 = base64_encode($bin2); // remove == , replace as space
-$finalkey64 = str_replace('=', '', $key64);
-
-// validation
-
-if (empty($finalkey64)){
-  if ($error !== 1){
-     // return 503 & JSON
-   http_response_code(503);
-   header("Content-Type: application/json");
-   $errorjson = array("Status"=>"503","Content"=>"Validation Failed!","Reason"=>"Did not provide Key ID | Key or Key ID | Key isn't complete.");
-   echo json_encode($errorjson);
-   
-      
-      exit;
-  }
-}
-
-/*
-// create JSON for keys 
-$keys[] = array("kty"=>"oct","k"=>$finalkey64,"kid"=>$finalkeyid64);
-
-// encode JSON
-$license = array("keys"=>$keys,"type"=>"temporary");
-
-// output JSON
-header("Content-Type: application/json");
-echo json_encode($license);
-*/
+echo $text;
 ?>
-
-
-{  
-    "keys":[  
-       {  
-          "kty":"oct",
-          "k":"<?php echo $finalkey64;?>",
-          "kid":"<?php echo $finalkeyid64;?>"
-       }
-    ],
-    "type":"temporary"
- }
- 
-
-
